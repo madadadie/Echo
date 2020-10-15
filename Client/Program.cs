c45c65dac5b09ced9023fd565b590be695b33d91
MD <danielledadie05@gmail.com>
2020-10-15 15:58:05 +0200

    Client and Server assignment 3

---
+++ Client/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+
+namespace Client
+{
+
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonPropertyName("cid")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    class ClientProgram
+    {
+        private const int Port = 5000;
+
+        /*public void Constraint_ConnectionWithoutRequest_ShouldConnect()
+        {
+            using var client = new TcpClient();
+            //connect to local IP Address
+            client.Connect(IPAddress.Loopback, port: 5000 );
+         
+            var stream = client.GetStream();
+
+            var data = Encoding.UTF8.GetBytes("Hello I'm a new client. Are you the server?");
+            // open the canal and send message
+            stream.Write(data);
+        }*/
+        public void Constraint_ConnectionWithoutRequest_ShouldConnect()
+        {
+            var client = Connect();
+            Assert.True(client.Connected);
+        }
+
+        public void Constraint_RequestWithoutMethod_MissingMethodError()
+        {
+            var client = Connect();
+
+            client.SendRequest("{}");
+
+            var response = client.ReadResponse();
+
+            Assert.Contains("missing method", response.Status.ToLower());
+        }
+
+        private static string UnixTimestamp()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+
+        private static TcpClient Connect()
+        {
+            var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, Port);
+            return client;
+        }
+    }
+
+    public static class Util
+    {
+        public static string ToJson(this object data)
+        {
+            return JsonSerializer.Serialize(data, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+
+        public static T FromJson<T>(this string element)
+        {
+            return JsonSerializer.Deserialize<T>(element, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+        }
+
+        public static void SendRequest(this TcpClient client, string request)
+        {
+            var msg = Encoding.UTF8.GetBytes(request);
+            client.GetStream().Write(msg, 0, msg.Length);
+        }
+
+        public static Response ReadResponse(this TcpClient client)
+        {
+            var strm = client.GetStream();
+            //strm.ReadTimeout = 250;
+            byte[] resp = new byte[2048];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = strm.Read(resp, 0, resp.Length);
+                    memStream.Write(resp, 0, bytesread);
+
+                } while (bytesread == 2048);
+
+                var responseData = Encoding.UTF8.GetString(memStream.ToArray());
+                return JsonSerializer.Deserialize<Response>(responseData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            }
+        }
+    }
+
+    }
+
+
+
+
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
@@ -6,7 +10,16 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create and connect to local IP Address
+            var server = new TcpListener(IPAddress.Loopback, 5000);
+            server.Start();
+            Console.WriteLine($"Server started!");
+            var i = 1;
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Console.WriteLine($"Accepted client {i}!");
+            }
         }
     }
 }
