023fd539ef97a937d191583a88e9179ab1566201
MD <danielledadie05@gmail.com>
2020-10-15 14:34:36 +0200

    Client sends a message and the server receives it

--- PrereqClient/ClientProgram.cs
+++ PrereqClient/ClientProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace PrereqClient
 {
@@ -11,6 +12,12 @@ namespace PrereqClient
             using var client = new TcpClient();
             //connect to local IP Address
             client.Connect(IPAddress.Loopback, port: 5000 );
+         
+            var stream = client.GetStream();
+
+            var data = Encoding.UTF8.GetBytes("Hello I'm a new client. Are you the server?");
+            // open the canal and send message
+            stream.Write(data);
         }
     }
 }
--- PrereqServer/ServerProgram.cs
+++ PrereqServer/ServerProgram.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace PrereqServer
 {
@@ -11,13 +13,26 @@ namespace PrereqServer
             //create and connect to local IP Address
             var server = new TcpListener(IPAddress.Loopback, 5000);
             server.Start();
+            Console.WriteLine($"Server started!");
             var i = 1;
             while (true)
             {
-               
                 var client = server.AcceptTcpClient();
-                Console.WriteLine($"Accepted client {i}");
+                Console.WriteLine($"Accepted client {i}!");
+                var stream = client.GetStream();
+                // need space to store what is reading
+                // allocate space corresponding to the message sent by client
+                byte[] data = new byte[client.ReceiveBufferSize];
+
+                //stream returns a number of bytes read from the client
+                var count = stream.Read(data);
+                // convert bytes into string 
+                var message = Encoding.UTF8.GetString(data, 0, count);
+
+                Console.WriteLine($"New message from client {i}: {message}");
+                
                 i += 1;
+            
             }
         }
     }
