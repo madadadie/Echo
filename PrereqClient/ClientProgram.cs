781383fe6d88b8c053a649d393d297fbbfc42819
MD <danielledadie05@gmail.com>
2020-10-16 16:29:26 +0200

    Multithreaded connection first part

--- PrereqClient/ClientProgram.cs
+++ PrereqClient/ClientProgram.cs
@@ -9,24 +9,27 @@ namespace PrereqClient
     {
         static void Main(string[] args)
         {
-            using var client = new TcpClient();
-            //connect to local IP Address
-            client.Connect(IPAddress.Loopback, port: 5000 );
-         
-            var stream = client.GetStream();
+            for (int j = 0; j < 5; j++)
+            {
+                using var client = new TcpClient();
+                //connect to local IP Address
+                client.Connect(IPAddress.Loopback, port: 5000);
 
-            var data = Encoding.UTF8.GetBytes("Hello I'm a new client. Are you the server?");
-            // open the canal and send message
-            stream.Write(data);
+                var stream = client.GetStream();
 
+                var data = Encoding.UTF8.GetBytes($"Hello I'm a new client {j}. Are you the server?");
+                // open the canal and send message
+                stream.Write(data);
 
-            //response from the server
-            data = new byte[client.ReceiveBufferSize];
-            //stream returns a number of bytes read from the client
-            var count = stream.Read(data);
-            // convert bytes into string 
-            var message = Encoding.UTF8.GetString(data, 0, count);
-            Console.WriteLine($"New message from server: {message}");
+                /*
+                //response from the server
+                data = new byte[client.ReceiveBufferSize];
+                //stream returns a number of bytes read from the client
+                var count = stream.Read(data);
+                // convert bytes into string 
+                var message = Encoding.UTF8.GetString(data, 0, count);
+                Console.WriteLine($"New message from server: {message}");*/
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,23 +3,92 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
 
 namespace Server
 {
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonPropertyName("cid")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    public class Request
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
+    }
+
+
+
     class ServerProgram
     {
+        private const int Port = 5000;
+        private static string UnixTimestamp()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        }
+        //public string Name { get; set; }
+
+
+        //method to create listener threads
+        public static void StartServerThread()
+        {
+            var server = new TcpListener(IPAddress.Loopback, Port);
+            server.Start();
+            Console.WriteLine($"Started server on port {Port}!");
+            var i = 1;
+            Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var client = server.AcceptTcpClient();
+                        var stream = client.GetStream();
+                        // need space to store what is reading
+                        // allocate space corresponding to the message sent by client
+                        byte[] data = new byte[client.ReceiveBufferSize];
+                        //stream returns a number of bytes read from the client
+                        var count = stream.Read(data);
+                        // convert bytes into string 
+                        var message = Encoding.UTF8.GetString(data, 0, count);
+                        Console.WriteLine($"Thread {i} -- message from new client : {message}");
+                        i += 1;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+            });
+            thread.Start();
+            //thread.Name = $" child{i}";
+        }
         static void Main(string[] args)
         {
-            //create and connect to local IP Address
-            var server = new TcpListener(IPAddress.Loopback, 5000);
-            server.Start();
-            Console.WriteLine($"Server started!");
-            var i = 1;
-            while (true)
-            {
-                var client = server.AcceptTcpClient();
-                Console.WriteLine($"Accepted client {i}!");
-            }
+            StartServerThread();
+            Thread.Sleep(300);
         }
+
+
     }
+
+   
+
+
+
 }
